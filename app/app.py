76263e7951183326ed372e441c162ee6df21a395
ashish ghimire <ashish.gh123@gmail.com>
2020-06-10 15:53:11 +0545

    make page for sentiment prediction

--- app/app.py
+++ app/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+import numpy as np
+import pandas as pd
+import re
+import os
+from numpy import array
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    return render_template("home.html")
+
